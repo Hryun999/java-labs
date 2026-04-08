a0453af4d420779ffc2aafdad6ee3b22c0b5479c
Hryun999 <ilahbrv2@gmail.com>
2026-04-08 21:11:05 +0300

    fo

---
+++ lab4/Calculator.java
@@ -0,0 +1,44 @@
+package com.mycompany.lb4;
+
+public class Calculator {
+    private static double function(double x) {
+        return 1.0 / x;
+    }
+    
+    public static double integral(double a, double b, double n) throws IntegralException {
+        if (n <= 0) {
+            throw new IntegralException("Шаг интегрирования должен быть положительным числом!");
+        }
+        
+        if (Math.abs(a - b) < 0.000001) {
+            return 0.0;
+        }
+        
+        double sum = 0.0;
+        
+        try {
+            int fullSteps = (int) Math.floor(Math.abs(b - a) / n);
+            
+            for (int i = 0; i < fullSteps; i++) {
+                double x1 = a + i * n;
+                double x2 = a + (i + 1) * n;
+                sum += (function(x1) + function(x2)) / 2.0 * n;
+            }
+            
+            double remainder = Math.abs(b - a) - fullSteps * n;
+            if (remainder > 0.000001) {
+                double lastX = a + fullSteps * n;
+                sum += (function(lastX) + function(b)) / 2.0 * remainder;
+            }
+            
+            if (b < a) {
+                sum = -sum;
+            }
+            
+        } catch (ArithmeticException e) {
+            throw new IntegralException("Арифметическая ошибка при вычислении интеграла!", e);
+        }
+        
+        return sum;
+    }
+}
---
+++ lab4/RecIntegral.java
@@ -0,0 +1,99 @@
+package com.mycompany.lb4;
+
+import java.io.Serializable;
+
+public class RecIntegral implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private double lowerBound;
+    private double upperBound;
+    private double step;
+    private double result;
+    
+    private static final double MIN_VALUE = 0.000001;
+    private static final double MAX_VALUE = 1000000;
+
+    public RecIntegral(double lowerBound, double upperBound, double step, double result) 
+            throws IntegralException {
+        
+        validateValue(lowerBound, "Нижний предел");
+        validateValue(upperBound, "Верхний предел");
+        validateValue(step, "Шаг");
+        
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+        this.step = step;
+        this.result = result;
+    }
+    
+    private void validateValue(double value, String fieldName) throws IntegralException {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw new IntegralException(
+                String.format("%s должен быть в диапазоне от %f до %f! (Введено: %f)", 
+                fieldName, MIN_VALUE, MAX_VALUE, value)
+            );
+        }
+    }
+    
+    private static void validateStaticValue(double value, String fieldName) throws IntegralException {
+        if (value < MIN_VALUE || value > MAX_VALUE) {
+            throw new IntegralException(
+                String.format("%s должен быть в диапазоне от %f до %f! (Введено: %f)", 
+                fieldName, MIN_VALUE, MAX_VALUE, value)
+            );
+        }
+    }
+    
+    public static void validateStep(double step) throws IntegralException {
+        if (step <= 0) {
+            throw new IntegralException("Шаг должен быть положительным числом! (Введено: " + step + ")");
+        }
+        validateStaticValue(step, "Шаг");
+    }
+    
+    public double getLowerBound() { return lowerBound; }
+    public double getUpperBound() { return upperBound; }
+    public double getStep() { return step; }
+    public double getResult() { return result; }
+    
+    public void setLowerBound(double lowerBound) throws IntegralException {
+        validateValue(lowerBound, "Нижний предел");
+        this.lowerBound = lowerBound;
+    }
+    
+    public void setUpperBound(double upperBound) throws IntegralException {
+        validateValue(upperBound, "Верхний предел");
+        this.upperBound = upperBound;
+    }
+    
+    public void setStep(double step) throws IntegralException {
+        validateValue(step, "Шаг");
+        this.step = step;
+    }
+    
+    public void setResult(double result) {
+        this.result = result;
+    }
+    
+    @Override
+    public String toString() {
+        return lowerBound + ";" + upperBound + ";" + step + ";" + result;
+    }
+    
+    public static RecIntegral fromString(String line) throws IntegralException {
+        String[] parts = line.split(";");
+        if (parts.length != 4) {
+            throw new IntegralException("Неверный формат строки: " + line);
+        }
+        try {
+            double lower = Double.parseDouble(parts[0]);
+            double upper = Double.parseDouble(parts[1]);
+            double step = Double.parseDouble(parts[2]);
+            double result = Double.parseDouble(parts[3]);
+            return new RecIntegral(lower, upper, step, result);
+        } catch (NumberFormatException e) {
+            throw new IntegralException("Ошибка парсинга чисел в строке: " + line);
+        }
+    }
+}
